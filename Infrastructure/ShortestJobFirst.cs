73d4c04f207c2267f38d2420891977cc8591d40a
steflk84 <steflk84@uwosh.edu>
2015-04-22 17:32:13 -0500

    added shortestJobFirst

---
+++ Infrastructure/ShortestJobFirst.cs
@@ -0,0 +1,42 @@
+﻿namespace Infrastructure
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ShortestJobFirst : Scheduler
+    {
+        public ShortestJobFirst(ProcessLoad processLoad) : base(processLoad)
+        {
+            this.ProcessesRunning = new List<Process>();
+            this.ProcessesToRun = new Queue<Process>(processLoad.Processes.OrderBy(p => p.ArrivalTime));
+        }
+
+        public List<Process> ProcessesRunning { get; set; }
+        private Process CurrentProcess { get; set; }
+        public Queue<Process> ProcessesToRun { get; set; }
+
+
+
+        public override Process GetProcessToRun(int currentTime)
+        {
+            while (this.ProcessesToRun.Any() && this.ProcessesToRun.Peek().ArrivalTime <= currentTime)
+            {
+                this.ProcessesRunning.Add(this.ProcessesToRun.Dequeue());
+
+            }
+
+            if (null == this.CurrentProcess)
+            {
+                this.CurrentProcess = this.ProcessesRunning.OrderBy(p => p.CurrentBurstCycle.CpuBurstTime).FirstOrDefault();
+                
+            }
+            else if (this.CurrentProcess.CurrentBurstCycle.CpuBurstIsComplete)
+            {
+                 this.ProcessesRunning.Remove(this.CurrentProcess);
+                 this.CurrentProcess = this.ProcessesRunning.OrderBy(p => p.CurrentBurstCycle.CpuBurstTime).FirstOrDefault();
+            } 
+
+            return this.CurrentProcess;
+        }
+    }
+}
--- InfrastructureTests/IntegrationTests.cs
+++ InfrastructureTests/IntegrationTests.cs
@@ -69,5 +69,33 @@
 
             var result = this.ProcessDispatcher.Start(scheduler, cpu, io);
         }
+    
+
+     [TestMethod]
+        public void ShortestJobFirst()
+        {
+            var bursts1 = new List<BurstCycle> { new BurstCycle { CpuBurstTime = 10, IoBurstTime = 15 }, new BurstCycle { CpuBurstTime = 4, IoBurstTime = 6 } };
+            var bursts2 = new List<BurstCycle> { new BurstCycle { CpuBurstTime = 8, IoBurstTime = 6 }, new BurstCycle { CpuBurstTime = 7, IoBurstTime = 0 } };
+            var bursts3 = new List<BurstCycle> { new BurstCycle { CpuBurstTime = 15, IoBurstTime = 4 }, new BurstCycle { CpuBurstTime = 8, IoBurstTime = 0 } };
+            var process1 = new Process(bursts1) { ArrivalTime = 0 };
+            var process2 = new Process(bursts2) { ArrivalTime = 3 };
+            var process3 = new Process(bursts3) { ArrivalTime = 10 };
+
+            var processLoad = new ProcessLoad
+            {
+                Processes = new List<Process>
+                {
+                    process1,
+                    process2,
+                    process3,
+                }
+            };
+
+            var scheduler = new ShortestJobFirst(processLoad);
+            var io = new Io();
+            var cpu = new Cpu(scheduler);
+
+            var result = this.ProcessDispatcher.Start(scheduler, cpu, io);
+        }
     }
 }
