0ae6d3da8d3c55e04bff80bd0772aa0fe9fb415d
fahrep58 <fahrep58@uwosh.edu>
2015-04-23 18:13:29 -0500

    FCFCS bar and avg time calced

--- CpuScheduler/MainWindow.xaml.cs
+++ CpuScheduler/MainWindow.xaml.cs
@@ -39,7 +39,14 @@ namespace CpuScheduler
                 
               
             };
-            
+
+            int last = numprocs + 1;
+            int arraycpu = 0;
+            int arrayio =0;
+
+            int time2 = 0;
+
+
             var scheduler = new FirstComeFirstServed(processLoad);
             var io = new Io();
             var cpu = new Cpu();
@@ -49,6 +56,8 @@ namespace CpuScheduler
             var cpuHistory = cpu.History;
             var ioHistory = io.History;
 
+          //  var cputime
+
             var runHistory = cpuHistory.Zip(ioHistory, (c, i) => new { CpuHistory = c, IoHistory = i });
 
             string header = String.Format("| {0,-12} | {1,-9:} | {2,-9:} |\n",
@@ -57,15 +66,41 @@ namespace CpuScheduler
             foreach (var historyItem in runHistory)
             {
                 var time = historyItem.CpuHistory.Item1;
+
+                time2 = time;
+
                 var cpuProcess = historyItem.CpuHistory.Item2;
+
+                 if (cpuProcess != -1)
+                 {
+                     arraycpu += 1;
+                 }
+                         
+
+
                 var ioProcess = historyItem.IoHistory.Item2;
 
+                if (ioProcess != -1)
+                 {
+                     arrayio += 1;
+                 }
 
                 string data = String.Format("| {0,-12:0000} | {1,-11} | {2,-10:G} |", time, cpuProcess, ioProcess);
                 Console.WriteLine(data);
+
+                
             }
+            double myballs = Convert.ToDouble(arraycpu) / Convert.ToDouble(time2)*100.0;
+            Int32 total = Convert.ToInt32(myballs);
+            pfcfs.Value = total;
+
+            int waittime = (time2 - arrayio)/numprocs;
+            lfcfswait.Content = waittime;
 
 
+
+
+            
         }
     }
 }
